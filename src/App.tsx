c4f26309898ed167c49e361ff3ef64976dd41382
Italo Simões <2d.italo@gmail.com>
2023-12-08 13:03:20 -0300

    BestSeller component criado e estilizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Home from "../src/components/Home/Home";
+import BestSeller from "./components/BestSeller/BestSeller";
 
 function App() {
   return (
     <>
       <Home />
+      <BestSeller />
     </>
   );
 }
---
+++ src/components/BestSeller/BestSeller.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./style.scss";
+
+import BestSellerCard01 from "../../assets/bestseller_card_01.png";
+import BestSellerCard02 from "../../assets/bestseller_card_02.png";
+import BestSellerCard03 from "../../assets/bestseller_card_03.png";
+
+const BestSeller = () => {
+  return (
+    <div className="bestseller_wrapper">
+      <h2 className="bestseller_title">Mais Vendidos</h2>
+      <p className="bestseller_text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+        doloremque eum aperiam nesciunt ratione ex eius quaerat, molestias
+        molestiae, voluptates dolore cupiditate, nemo perferendis eveniet
+        veritatis libero. Asperiores, nisi nostrum.
+      </p>
+
+      <div className="bestseller_cards_wrapper">
+        <div className="bestseller_card_container">
+          <img src={BestSellerCard01} alt="" />
+          <h3>Prato 01</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="bestseller_card_container">
+          <img src={BestSellerCard02} alt="" />
+          <h3>Prato 02</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="bestseller_card_container">
+          <img src={BestSellerCard03} alt="" />
+          <h3>Prato 03</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
