2e75a64d833eaf4ff514758e7890d4443ca427c3
Italo Simões <2d.italo@gmail.com>
2023-12-08 12:30:12 -0300

    home criada e estilizada

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.scss";
+
+// images
+
+import HomeBackground from "../../assets/images/restaurante_home_bg.png";
+import HomeDetail from "../../assets/images/restaurante_home_detail_2.png";
+
+const Home = () => {
+  return (
+    <div className="home_wrapper">
+      <img className="home_background" src={HomeBackground} alt="" />
+      <img className="home_detail" src={HomeDetail} alt="" />
+      <div className="home_text_container">
+        <div className="home_title">Restaurante</div>
+        <div className="home_text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ratione
+          excepturi porro ullam ducimus quam.
+        </div>
+        <a className="submit_button">FAÇA SEU PEDIDO</a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
