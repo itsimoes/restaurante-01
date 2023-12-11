45a47f8636f1c287880963d3d47224daf4aa4f7f
Italo Simões <2d.italo@gmail.com>
2023-12-11 10:27:35 -0300

    correção: alteração de textos para portugues

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -4,14 +4,14 @@ import "./style.scss";
 const Subscribe = () => {
   return (
     <div className="subscribe_wrapper">
-      <h3 className="subscribe_title">Subscribe</h3>
+      <h3 className="subscribe_title">Inscreva-se!</h3>
       <div className="subscribe_text">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
         consequuntur eaque dicta ea eligendi ut culpa reiciendis accusantium.
       </div>
       <form className="subscribe_form" action="">
         <input id="subscribe_input" type="text" placeholder="Seu Email" />
-        <input id="subscribe_submit_button" type="submit" value="SUBSCRIBE" />
+        <input id="subscribe_submit_button" type="submit" value="INSCREVA-SE" />
       </form>
     </div>
   );
