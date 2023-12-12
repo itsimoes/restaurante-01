6ae88eb8dbb354ee3083c3753b5fc3f50b73798e
Italo Simões <2d.italo@gmail.com>
2023-12-12 15:33:55 -0300

    responsividade 1080p corrigida no Subscribe component

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -4,15 +4,21 @@ import "./style.scss";
 const Subscribe = () => {
   return (
     <div id="subscribe" className="subscribe_wrapper">
-      <h3 className="subscribe_title">Inscreva-se!</h3>
-      <div className="subscribe_text">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
-        consequuntur eaque dicta ea eligendi ut culpa reiciendis accusantium.
+      <div className="subscribe_container">
+        <h3 className="subscribe_title">Inscreva-se!</h3>
+        <div className="subscribe_text">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+          consequuntur eaque dicta ea eligendi ut culpa reiciendis accusantium.
+        </div>
+        <form className="subscribe_form" action="">
+          <input id="subscribe_input" type="text" placeholder="Seu Email" />
+          <input
+            id="subscribe_submit_button"
+            type="submit"
+            value="INSCREVA-SE"
+          />
+        </form>
       </div>
-      <form className="subscribe_form" action="">
-        <input id="subscribe_input" type="text" placeholder="Seu Email" />
-        <input id="subscribe_submit_button" type="submit" value="INSCREVA-SE" />
-      </form>
     </div>
   );
 };
