6387d17f3688f6b5afbd120da5b808871eb2f179
Italo Simões <2d.italo@gmail.com>
2023-12-12 15:23:30 -0300

    responsividade 1080p corrigida no Highlights component

--- src/components/Highlights/Statistics.tsx
+++ src/components/Highlights/Statistics.tsx
@@ -3,28 +3,30 @@ import React from "react";
 const Statistics = () => {
   return (
     <div className="statistics_wrapper">
-      <h3 className="highlights_title">Nossos números</h3>
-      <p id="statistics_text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-        asperiores quas doloribus id. Odit accusamus hic molestias adipisci.
-        Recusandae beatae temporibus exercitationem.
-      </p>
-      <div className="statistics_cards_container">
-        <div className="statistics_card">
-          <span>123</span>          
-          UNIDADES
-        </div>
-        <div className="statistics_card">
-          <span>100</span>          
-          CHEF
-        </div>
-        <div className="statistics_card">
-          <span>300</span>          
-          MENU
-        </div>
-        <div className="statistics_card">
-          <span>30</span>          
-          CIDADES
+      <div className="statistics_container">
+        <h3 className="highlights_title">Nossos números</h3>
+        <p id="statistics_text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+          asperiores quas doloribus id. Odit accusamus hic molestias adipisci.
+          Recusandae beatae temporibus exercitationem.
+        </p>
+        <div className="statistics_cards_container">
+          <div className="statistics_card">
+            <span>123</span>
+            UNIDADES
+          </div>
+          <div className="statistics_card">
+            <span>100</span>
+            CHEF
+          </div>
+          <div className="statistics_card">
+            <span>300</span>
+            MENU
+          </div>
+          <div className="statistics_card">
+            <span>30</span>
+            CIDADES
+          </div>
         </div>
       </div>
     </div>
